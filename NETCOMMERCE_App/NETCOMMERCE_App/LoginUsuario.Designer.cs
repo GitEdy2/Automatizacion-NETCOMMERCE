b05028a40d205db32df5e9c2ebc8bd4508f495d4
GitEdy2 <clanuzumaki22@gmail.com>
2018-06-07 10:47:40 -0500

    Diseño primario de cuadro de login de usuario.

---
+++ NETCOMMERCE_App/NETCOMMERCE_App/LoginUsuario.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace NETCOMMERCE_App
+{
+    partial class LoginUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.PBNetcommerce = new System.Windows.Forms.PictureBox();
+            this.pbQR = new System.Windows.Forms.PictureBox();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.lblLoginUsuario = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.PBNetcommerce)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbQR)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(165, 160);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(165, 20);
+            this.txtUsuario.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(165, 228);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(165, 20);
+            this.txtPassword.TabIndex = 1;
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuario.Location = new System.Drawing.Point(109, 163);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(54, 13);
+            this.lblUsuario.TabIndex = 2;
+            this.lblUsuario.Text = "Usuario:";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.Location = new System.Drawing.Point(99, 228);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(65, 13);
+            this.lblPassword.TabIndex = 3;
+            this.lblPassword.Text = "Password:";
+            // 
+            // PBNetcommerce
+            // 
+            this.PBNetcommerce.Location = new System.Drawing.Point(450, 87);
+            this.PBNetcommerce.Name = "PBNetcommerce";
+            this.PBNetcommerce.Size = new System.Drawing.Size(248, 102);
+            this.PBNetcommerce.TabIndex = 4;
+            this.PBNetcommerce.TabStop = false;
+            // 
+            // pbQR
+            // 
+            this.pbQR.Location = new System.Drawing.Point(450, 228);
+            this.pbQR.Name = "pbQR";
+            this.pbQR.Size = new System.Drawing.Size(248, 110);
+            this.pbQR.TabIndex = 5;
+            this.pbQR.TabStop = false;
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(165, 294);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(133, 23);
+            this.btnIngresar.TabIndex = 6;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            // 
+            // lblLoginUsuario
+            // 
+            this.lblLoginUsuario.AutoSize = true;
+            this.lblLoginUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLoginUsuario.Location = new System.Drawing.Point(50, 52);
+            this.lblLoginUsuario.Name = "lblLoginUsuario";
+            this.lblLoginUsuario.Size = new System.Drawing.Size(349, 46);
+            this.lblLoginUsuario.TabIndex = 7;
+            this.lblLoginUsuario.Text = "LOGIN USUARIO";
+            // 
+            // LoginUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblLoginUsuario);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.pbQR);
+            this.Controls.Add(this.PBNetcommerce);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsuario);
+            this.Name = "LoginUsuario";
+            this.Text = "LoginUsuario";
+            ((System.ComponentModel.ISupportInitialize)(this.PBNetcommerce)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbQR)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.PictureBox PBNetcommerce;
+        private System.Windows.Forms.PictureBox pbQR;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.Label lblLoginUsuario;
+    }
+}
